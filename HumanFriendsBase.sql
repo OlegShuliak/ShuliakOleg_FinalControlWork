3be33b41e9b0ec2c24e7a942e42fa7d094a2961a
Oleg  Shuliak <nformals@gmail.com>
2023-12-28 23:18:53 +0300

    Rebuild tables for task 8, 9, 10, add connections between tables

--- HumanFriendsBase.sql
+++ HumanFriendsBase.sql
@@ -11,60 +11,62 @@ CREATE TABLE human_friend(
 CREATE TABLE pet(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     type_of_animal_id INT NOT NULL,
-    kind_of_animal VARCHAR(45) NOT NULL);
+    kind_of_animal VARCHAR(45) NOT NULL,
+    FOREIGN KEY (type_of_animal_id) REFERENCES human_friend (id) ON DELETE CASCADE ON UPDATE CASCADE);
 
 CREATE TABLE pack_animal(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     type_of_animal_id INT NOT NULL,
-    kind_of_animal VARCHAR(45) NOT NULL);
+    kind_of_animal VARCHAR(45) NOT NULL,
+    FOREIGN KEY (type_of_animal_id) REFERENCES human_friend (id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE hamster(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pet(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE dog(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pet(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE cat(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pet(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE horse(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pack_animal(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE donkey(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pack_animal(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 CREATE TABLE camel(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    type_of_animal_id INT NOT NULL,
-    kind_of_animal_id VARCHAR(45) NOT NULL,
+    kind_of_animal_id INT NOT NULL,
     animal_name VARCHAR(45) NOT NULL,
     skill VARCHAR(45) NOT NULL,
-    date_of_birth DATE NOT NULL);
+    date_of_birth DATE NOT NULL,
+    FOREIGN KEY (kind_of_animal_id) REFERENCES pack_animal(id) ON DELETE CASCADE ON UPDATE CASCADE);
     
 -- Задание 9
     INSERT INTO human_friend (type_of_animal)
@@ -84,35 +86,35 @@ VALUES
 (2, 'Осел'), 
 (2, 'Верблюд');
   
-   INSERT INTO hamster (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+   INSERT INTO hamster (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(1, 1, 'Хома', 'Стоять', '2022-08-15'),
-(1, 1, 'Рон', 'Перевернуться', '2023-02-01');
+(1, 'Хома', 'Стоять', '2022-08-15'),
+(1, 'Рон', 'Перевернуться', '2023-02-01');
 
-  INSERT INTO dog (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+  INSERT INTO dog (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(1, 2, 'Жучка', 'Сидеть', '2020-05-19'),
-(1, 2, 'Рэкс', 'Лежать', '2018-12-25');
+(2, 'Жучка', 'Сидеть', '2020-05-19'),
+(2, 'Рэкс', 'Лежать', '2018-12-25');
   
-  INSERT INTO cat (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+  INSERT INTO cat (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(1, 3, 'Бэтмэн', 'Лежать', '2019-02-09'),
-(1, 3, 'Сноу', 'Стоять', '2022-07-14');
+(3, 'Бэтмэн', 'Лежать', '2019-02-09'),
+(3, 'Сноу', 'Стоять', '2022-07-14');
   
-  INSERT INTO horse (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+  INSERT INTO horse (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(2, 1, 'Апполон', 'Шагом', '2023-05-18'),
-(2, 1, 'Гамлет', 'Рысь', '2019-01-26');
+(1, 'Апполон', 'Шагом', '2023-05-18'),
+(1, 'Гамлет', 'Рысь', '2019-01-26');
   
-  INSERT INTO donkey (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+  INSERT INTO donkey (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(2, 2, 'Пуля', 'Шагом', '2018-12-05'),
-(2, 2, 'Руди', 'Шагом', '2021-04-04');
+(2, 'Пуля', 'Шагом', '2018-12-05'),
+(2, 'Руди', 'Шагом', '2021-04-04');
   
-  INSERT INTO camel (type_of_animal_id, kind_of_animal_id, animal_name, skill, date_of_birth)
+  INSERT INTO camel (kind_of_animal_id, animal_name, skill, date_of_birth)
 VALUES
-(2, 3, 'Миша', 'Шагом', '2019-7-29'),
-(2, 3, 'Артур', 'Рысь', '2018-03-05');
+(3, 'Миша', 'Шагом', '2019-7-29'),
+(3, 'Артур', 'Рысь', '2018-03-05');
     
 -- Задание 10
 SELECT * FROM camel;
@@ -122,6 +124,7 @@ SELECT * FROM camel;
 SELECT * FROM horse
 UNION SELECT * FROM donkey; -- объединяем таблицы лошадей и ослов
     
+
 
 	
 
